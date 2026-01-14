8073f140aa92aa20a1035b8e5cf6c7d0aba82507
Viktor Shchelochkov <hi@hloth.dev>
2026-01-14 19:33:38 +0100

    Resize dropdown menu items

--- src/lib/components/ui/dropdown-menu/dropdown-menu-content.svelte
+++ src/lib/components/ui/dropdown-menu/dropdown-menu-content.svelte
@@ -22,7 +22,7 @@
 		data-slot="dropdown-menu-content"
 		{sideOffset}
 		class={cn(
-			"z-50 max-h-(--bits-dropdown-menu-content-available-height) min-w-[8rem] origin-(--bits-dropdown-menu-content-transform-origin) overflow-x-hidden overflow-y-auto rounded-[22px] border bg-popover/30 p-1.5 text-popover-foreground shadow-md backdrop-blur-3xl transition-[scale] outline-none data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-end-2 data-[side=right]:slide-in-from-start-2 data-[side=top]:slide-in-from-bottom-2 data-[state=closed]:scale-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:scale-100 data-[state=open]:animate-in data-[state=open]:fade-in-0",
+			"z-50 max-h-(--bits-dropdown-menu-content-available-height) min-w-[8rem] origin-(--bits-dropdown-menu-content-transform-origin) overflow-x-hidden overflow-y-auto rounded-3xl border bg-popover/30 p-1.5 text-popover-foreground shadow-md backdrop-blur-3xl transition-[scale] outline-none data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-end-2 data-[side=right]:slide-in-from-start-2 data-[side=top]:slide-in-from-bottom-2 data-[state=closed]:scale-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:scale-100 data-[state=open]:animate-in data-[state=open]:fade-in-0",
 			className
 		)}
 		{...restProps}
